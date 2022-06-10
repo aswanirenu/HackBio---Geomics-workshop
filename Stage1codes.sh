c66d0e5e964a65ff8b57db04943255f73ce338c4
aswanirenu <106551645+aswanirenu@users.noreply.github.com>
2022-06-10 22:26:58 +0530

    Update Stage1codes.sh

--- Stage1codes.sh
+++ Stage1codes.sh
@@ -1,6 +1,13 @@
  #!/bin/bash
+ 
 cd team_darwin
+#download the file DNA.fa
+wget https://raw.githubusercontent.com/HackBio-Internship/wale-home-tasks/main/DNA.fa
+
+#Counting the number of sequences in DNA.fa 
 grep -c ">" DNA.fa
+
+#One-line command in Bash to get the total A, T, G & C counts for all the sequences in the file above
 grep -Eo 'A|T|G|C' DNA.fa | sort | uniq -c | awk '{print $2": "$1}'
 
 #setting up miniconda
@@ -11,9 +18,14 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 conda config --set auto_activate_base false
 conda activate base
-#download and installation of fastqc
+
+#Installation of fastqc
+sudo apt-get install fastqc
+
 mkdir output
 cd output
+
+#downloading the required datasets
 mkdir Qc_report
 fastqc Baxter_R1.fastq.gz -o Qc_report
 ls Qc_report
