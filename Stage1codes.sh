4a4b84fa6c6f46bcfaee180a6fc31d3d6526cb90
aswanirenu <106551645+aswanirenu@users.noreply.github.com>
2022-06-13 00:10:39 +0530

    Update Stage1codes.sh

--- Stage1codes.sh
+++ Stage1codes.sh
@@ -19,8 +19,11 @@ source ~/.bashrc
 conda config --set auto_activate_base false
 conda activate base
 
-#Installation of fastqc
-sudo apt-get install fastqc
+#Installation of software
+sudo apt-get -y install fastqc
+sudo apt-get -y install fastp
+sudo apt-get -y install bwa
+sudo apt-get -y install samtools
 
 #creating the output folder
 mkdir output
@@ -62,6 +65,69 @@ fastqc Drysdale_R1.fastq.gz -o Qc_report
 ls Qc_report
 fastqc Drysdale_R2.fastq.gz -o Qc_report
 ls Qc_report
+
+#implementing fastp
+
+#download trim.sh script for implementing fastp for all sequences
+wget https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/trim.sh
+
+bash trim.sh
+
+#renaming
+mv \Qc_reads trimmed_reads
+
+#implementing BWA (burrow wheelar alignment)
+
+mkdir references
+cd references
+
+#download the required reference genome
+wget https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/references/reference.fasta
+
+#download script for performing bwa
+wget https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/aligner.sh
+
+bash aligner.sh
+
+#implementing samtools (manipulating sam/bam/cram files)
+
+cd alignment_map
+
+#converting sam to bam
+#samtools view -S -b filename.bam > filename.sam
+
+samtools view -S -b Alsen.bam > Alsen.sam
+samtools view -S -b Baxter.bam > Baxter.sam
+samtools view -S -b Chara.bam > Chara.sam
+samtools view -S -b Drysdale.bam > Drysdale.sam
+
+#view bam files
+samtools view ACBarrie.bam | less
+samtools view Alsen.bam | less
+samtools view Baxter.bam | less
+samtools view Chara.bam | less
+samtools view Drysdale.bam | less
+
+#sort according to ascending order
+samtools sort ACBarrie.bam -o sorted_ACBarrie.bam
+samtools sort Alsen.bam -o sorted_Alsen.bam
+samtools sort Baxter.bam -o sorted_Baxter.bam
+samtools sort Chara.bam -o sorted_Chara.bam
+samtools sort Drysdale.bam -o sorted_Drysdale.bam
+
+#visualize to check if bam files are sorted
+samtools view sorted_ACBarrie.bam | head -n 5
+samtools view sorted_Alsen.bam | head -n 5
+samtools view sorted_Baxter.bam | head -n 5
+samtools view sorted_Chara.bam | head -n 5
+samtools view sorted_Drysdale.bam | head -n 5
+
+cd raw_reads
+
+#move alignment_map and repaired folders to output folder
+mv -v alignment_map/ output/
+mv -v repaired/ output/
+
 
 
 
